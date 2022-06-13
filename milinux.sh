860235d7ab118299f883693dac180f71b1804413
digiraldo <disaned1@gmail.com>
2022-06-12 22:31:32 -0500

    Cambios

--- milinux.sh
+++ milinux.sh
@@ -1,9 +1,63 @@
 #!/bin/bash
 
+# Función para leer la entrada del usuario con un mensaje
+function read_with_prompt {
+  variable_name="$1"
+  prompt="$2"
+  default="${3-}"
+  unset $variable_name
+  while [[ ! -n ${!variable_name} ]]; do
+    read -p "$prompt: " $variable_name < /dev/tty
+    if [ ! -n "`which xargs`" ]; then
+      declare -g $variable_name=$(echo "${!variable_name}" | xargs)
+    fi
+    declare -g $variable_name=$(echo "${!variable_name}" | head -n1 | awk '{print $1;}')
+    if [[ -z ${!variable_name} ]] && [[ -n "$default" ]] ; then
+      declare -g $variable_name=$default
+    fi
+    echo -n "$prompt : ${!variable_name} -- aceptar? (y/n)"
+    read answer < /dev/tty
+    if [ "$answer" == "${answer#[Yy]}" ]; then
+      unset $variable_name
+    else
+      echo "$prompt: ${!variable_name}"
+    fi
+  done
+}
+
+# Colores del terminal
+BLACK=$(tput setaf 0)
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+LIME_YELLOW=$(tput setaf 190)
+BLUE=$(tput setaf 4)
+MAGENTA=$(tput setaf 5)
+CYAN=$(tput setaf 6)
+WHITE=$(tput setaf 7)
+BRIGHT=$(tput bold)
+NORMAL=$(tput sgr0)
+BLINK=$(tput blink)
+REVERSE=$(tput smso)
+UNDERLINE=$(tput smul)
+
+# Imprime una línea con color usando códigos de terminal
+Print_Style() {
+  printf "%s\n" "${2}$1${NORMAL}"
+}
 
 # https://www.digitalocean.com/community/tutorials/how-to-install-linux-apache-mysql-php-lamp-stack-on-ubuntu-22-04
 
-echo -e "\n\nActualizando paquetes Apt y cargando ultimos paquetes\n"
+
+echo -n "¿Iniciar instalaciones? (y/n)"
+    read answer < /dev/tty
+    if [ "$answer" != "${answer#[Yy]}" ]; then
+      # Recuperar mundo e instalarlo en el servidor
+        echo "========================================================================="
+        Print_Style "Iniciando Recuperación con back.sh" "$YELLOW"
+        echo "========================================================================="
+        sleep 3s
+        echo -e "\n\nActualizando paquetes Apt y cargando ultimos paquetes\n"
 sleep 2s
 sudo apt-get update -y && sudo apt-get upgrade -y
 
@@ -159,13 +213,30 @@ sudo apt-get update
 sudo apt-get install -y powershell
 # Start PowerShell
 pwsh
+    fi
+
+
+
+
+Print_Style "Reinicias password en mysql" "$RED"
+    echo -n "¿Desea reiniciar password en mysql? (y/n)"
+    read answer < /dev/tty
+    if [ "$answer" != "${answer#[Yy]}" ]; then
+      # Recuperar mundo e instalarlo en el servidor
+        echo "========================================================================="
+        Print_Style "Iniciando Recuperación con back.sh" "$YELLOW"
+        echo "========================================================================="
+        sleep 3s
+        /bin/bash dirname/minecraftbe/servername/back.sh
+    fi
+
 
 
 echo -e "\n\n Detenga el servidor MySQL\n"
 sudo systemctl stop mysql.service
 
 echo -e "\n\n Estado del servidor MySQL\n"
-udo systemctl status mysql.service
+sudo systemctl status mysql.service
 sleep 3s
 
 echo -e "\n\n Omita las tablas de subvenciones y la creación de redes\n"
@@ -175,15 +246,11 @@ echo -e "\n\n Inicie el servicio MySQL\n"
 sudo systemctl start mysql.service
 
 echo -e "\n\n Confirme el estado del servidor MySQL\n"
-udo systemctl status mysql.service
+sudo systemctl status mysql.service
 
 
 
-echo -e "\n\n
-flush privileges\n
-USE mysql\n
-ALTER USER  ‘root’@‘localhost’ IDENTIFIED BY ‘root1234’\n
-quit\n"
+echo -e "\n\n flush privileges\n USE mysql\n ALTER USER ‘root’@‘localhost’ IDENTIFIED BY ‘root1234’\n quit\n"
 
 echo -e "\n\n inicie sesión en el shell de MySQL\n"
 sudo mysql -u root
